af5c32a1fe3a8f046207057a8afbefec6d4474a7
Cristian <cristian.mambriani@yahoo.com.ar>
2023-02-02 15:02:48 -0300

    Added basic walljump functionality

--- Assets/Entities/Player/PlayerMovement.cs
+++ Assets/Entities/Player/PlayerMovement.cs
@@ -4,18 +4,31 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField]
-    private float movementSpeed;
+    private float movementSpeed = 10;
     [SerializeField]
-    private float jumpHeight;
+    private float jumpHeight = 20;
+    [SerializeField]
+    private float wallJumpCooldown = 0.5f;
+    private float wallJumpTimer = 0;
+    [SerializeField]
+    private float wallJumpHorizontalForce = 1250;
+    [SerializeField]
+    private float wallJumpVerticalForce = 750;
+    [SerializeField]
+    private float baseGravityScale = 5;
+    [SerializeField]
+    private float wallHangGravityScale = 1;
 
-    private bool isGrounded;
+    private float horizontalMovement;
 
     private Rigidbody2D rBody;
     private BoxCollider2D bCol;
 
-    [Header("Visuals")]
+    [Header("Collisions")]
     [SerializeField]
     private LayerMask groundLayer;
+    [SerializeField]
+    private LayerMask wallLayer;
 
     private Animator animator;
 
@@ -25,6 +38,8 @@ public class PlayerMovement : MonoBehaviour
     private float timeLooking;
     [SerializeField]
     private GameObject forwardCameraAnchor;
+    [SerializeField]
+    private GameObject backwardCameraAnchor;
     [SerializeField]
     private GameObject topCameraAnchor;
     [SerializeField]
@@ -43,9 +58,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        horizontalMovement = Input.GetAxis("Horizontal");
 
-        rBody.velocity = new Vector2(horizontalMovement * movementSpeed, rBody.velocity.y);
+        if(wallJumpTimer <= 0)
+            rBody.velocity = new Vector2(horizontalMovement * movementSpeed, rBody.velocity.y);
 
         if (horizontalMovement > 0.01f)
             transform.localScale = Vector3.one;
@@ -53,16 +69,72 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         
 
-        isGrounded = CheckGrounded();
+        if (CheckOnWall())
+        {
+            rBody.gravityScale = wallHangGravityScale;
+        }
+        else
+        {
+            rBody.gravityScale = baseGravityScale;
+        }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
 
         MoveCamera();
 
-        if(isGrounded)
+        NotifyAnimator();
+
+        AdvanceTimers();
+    }
+
+
+    private void Jump()
+    {
+        if(CheckGrounded())
+            rBody.velocity = new Vector2(rBody.velocity.x, jumpHeight);
+
+        if (CheckOnWall() && wallJumpTimer <= 0 && !CheckGrounded())
+        {
+            if (horizontalMovement != 0)
+                rBody.velocity =new Vector2(-Mathf.Sign(transform.localScale.x) * wallJumpHorizontalForce, wallJumpVerticalForce);
+            else
+                rBody.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * wallJumpHorizontalForce * 2, wallJumpVerticalForce);
+
+            wallJumpTimer = wallJumpCooldown;
+            transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }   
+    }
+
+    private bool CheckGrounded()
+    {
+        if (Physics2D.BoxCast(bCol.bounds.center, bCol.bounds.size, 0, Vector2.down, 0.1f, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool CheckOnWall()
+    {
+        if(Physics2D.BoxCast(bCol.bounds.center, bCol.bounds.size,0,new Vector2(transform.localScale.x, 0), 0.1f, wallLayer) && !CheckGrounded())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }  
+    }
+
+    private void NotifyAnimator()
+    {
+        if (CheckGrounded())
             animator.SetBool("Grounded", true);
         else
             animator.SetBool("Grounded", false);
@@ -82,35 +154,22 @@ public class PlayerMovement : MonoBehaviour
         else
             animator.SetBool("Cusping", false);
 
-        if (!isGrounded && rBody.velocity.y < -0.01f)
+        if (!CheckGrounded() && rBody.velocity.y < -0.01f)
             animator.SetBool("Falling", true);
         else
             animator.SetBool("Falling", false);
-
-    }
-
-
-    private void Jump()
-    {
-        if(isGrounded)
-            rBody.velocity = new Vector2(rBody.velocity.x, jumpHeight);
-    }
-
-    private bool CheckGrounded()
-    {
-        if (Physics2D.BoxCast(bCol.bounds.center, bCol.bounds.size, 0, Vector2.down, 0.1f, groundLayer))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 
     private void MoveCamera()
     {
         float verticalMovement = Input.GetAxis("Vertical");
+
+        if (CheckOnWall())
+        {
+            timeLooking = 0;
+            mainCameraControl.LerpLookAt(backwardCameraAnchor.transform.position);
+            return;
+        }
 
         if (rBody.velocity.magnitude > 0.1f)
         {
@@ -136,5 +195,11 @@ public class PlayerMovement : MonoBehaviour
             else
                 mainCameraControl.LerpLookAt(bottomCameraAnchor.transform.position);
         }
+    }
+
+    private void AdvanceTimers()
+    {
+        if (wallJumpTimer > 0)
+            wallJumpTimer -= Time.deltaTime;
     }
 }
